16e5a6887dae792ae72da062560c64396f0751fb
Thijs Triemstra <info@collab.nl>
2016-02-06 19:13:56 +0100

    only log r.js errors during tests

--- require_license/tests/resources/js/app.build.js
+++ require_license/tests/resources/js/app.build.js
@@ -136,5 +136,15 @@
     // skip the optimization of those files, to speed up builds. Set this value
     // to true if you want to skip optimizing those other non-build layer JS
     // files.
-    skipDirOptimize: true
+    skipDirOptimize: true,
+
+    // Sets the logging level. It is a number. If you want "silent" running,
+    // set logLevel to 4. From the logger.js file:
+    // TRACE: 0,
+    // INFO: 1,
+    // WARN: 2,
+    // ERROR: 3,
+    // SILENT: 4
+    // Default is 0.
+    logLevel: 3,
 })
